8fb918dfed7a688cb705ebfc30da30616b576199
captainlomik <nastiy248@mail.ru>
2024-03-28 00:37:28 +0300

    fix popup

--- src/app/settings-popup/settings-popup.component.ts
+++ src/app/settings-popup/settings-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -8,10 +9,17 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class SettingsPopupComponent {
   unitValues = ['%', 'px']
+  methods = ['']
   unit!: string
+  sizeForm!:FormGroup
 
   constructor(public dialogRef: MatDialogRef<SettingsPopupComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
+
+      this.sizeForm = new FormGroup({
+        "with": new FormControl(),
+        "height":  new FormControl()
+      })
   }
 
   closepopup() {
